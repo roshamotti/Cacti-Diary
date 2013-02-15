6df64604b42fefcbdca214bbfa56a7fee7034d9f
Rosha <rosha.motti@gmail.com>
2013-02-15 13:30:49 -0800

    testing a new loop

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php get_header();?>
 		<section>
 			<img src="<?php bloginfo('stylesheet_directory');?>/images/hero.png" />
-			<?php if (have_posts()) : while (have_posts()): the_post();?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			  <section>
+			  <h1><a href="<?php the_permalink(); ?>" title="For More Info on <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+
+			<?php the_content(); ?>
+			  </section>
 			<?php endwhile; else: ?>
-				<p><?php _e('Sorry, no posts matched your criteria.')?></p>
-			<?php endif; ?>
-			
-			<section>
-				<?php the_content();?>
-										<!-- <p>Pressed and Squeezed is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
-										<p>We also make pancakes on the side.</p> -->
-									</section>
+			<!-- <?php if (have_posts()) : while (have_posts()): the_post();?>
+					<?php endwhile; else: ?>
+						<p><?php _e('Sorry, no posts matched your criteria.')?></p>
+					<?php endif; ?>
+					
+					<section>
+						<?php the_content();?>
+												<!-- <p>Pressed and Squeezed is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
+												<p>We also make pancakes on the side.</p> 
+											</section> -->
+											
 						
 		</section>
 <?php get_footer(); ?>
+
+
+
+
