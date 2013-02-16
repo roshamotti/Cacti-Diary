753240e7cd06a03014504860bb3598bd98dfce16
Rosha <rosha.motti@gmail.com>
2013-02-15 17:51:03 -0800

    testing again

--- index.php
+++ index.php
@@ -8,16 +8,7 @@
 			<?php the_content(); ?>
 			  </section>
 			<?php endwhile; else: ?>
-			<!-- <?php if (have_posts()) : while (have_posts()): the_post();?>
-					<?php endwhile; else: ?>
-						<p><?php _e('Sorry, no posts matched your criteria.')?></p>
-					<?php endif; ?>
-					
-					<section>
-						<?php the_content();?>
-												<!-- <p>Pressed and Squeezed is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
-												<p>We also make pancakes on the side.</p> 
-											</section> -->
+
 											
 						
 		</section>
